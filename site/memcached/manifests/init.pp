0a8850ea22eed7b16135ebb58f846102d41a6061
DonalKeane <donal.keane@ie.ibm.com>
2016-04-27 15:57:34 +0100

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,19 +1,21 @@
-file { 'memcached':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  path => '/etc/sysconfig/memcached'
-  source => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached],
-}
-
-package { 'memcached':
-  ensure => present,
-}
-
-service { 'memcached':
-  ensure => running,
-  enable => true,
-  subscribe => File['memcached],
+class memcached {
+  file { 'memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    path => '/etc/sysconfig/memcached'
+    source => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached],
+  }
+  
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['memcached],
+  }
 }
